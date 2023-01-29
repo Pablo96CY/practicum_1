e052ae56f53f4abc243632abf519fecda9d2f418
Bolshakov Pavel <bolshakovpa@msk.bcs.ru>
2023-01-29 19:01:40 +0300

    add ingredients scroll with tab dependencies

--- src/components/BurgerIngredients/burgerIngredients.jsx
+++ src/components/BurgerIngredients/burgerIngredients.jsx
@@ -1,14 +1,22 @@
-import React, { useMemo } from "react";
+import React, { useState, useMemo, useRef } from "react";
 import { useSelector } from 'react-redux';
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import style from "./style.module.css";
 import Card from "../BurgerCard/burgerCard";
-import Tabs from "../Tabs/tabs";
 import localize from "../../utils/localize";
 import { Ingredients } from "../../utils/enum";
 
 const BurgerIngredients = () => {
   const data  = useSelector((state) => state.burgerIngredients);
+
+  const [activeTab, setActiveTab] = useState(Ingredients.bun);
+
+  const bunTab = useRef(null);
+
+  const sauceTab = useRef(null);
+
+  const mainTab = useRef(null);
 
   const buns = useMemo(() => {
     return data.data.filter(item => item.type === Ingredients.bun);
@@ -20,16 +28,71 @@ const BurgerIngredients = () => {
   
   const mains = useMemo(() => {
     return data.data.filter(item => item.type === Ingredients.main);
-  }, [data]);
+  }, [data]);  
+
+  const scrollOnBunTabClick = () => {
+    bunTab.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  const scrollOnSauceTabClick = () => {
+    sauceTab.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  const scrollOnMainTabClick = () => {
+    mainTab.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  const onScroll = (event) => {
+    if (event.target.scrollTop <= 270) {
+      setActiveTab(Ingredients.bun);
+    }
+    if (event.target.scrollTop > 270 && event.target.scrollTop <= 790) {
+      setActiveTab(Ingredients.sauce);
+    }
+    if (event.target.scrollTop > 790){
+      setActiveTab(Ingredients.main);
+    }
+  }
 
   return (
     <section className={style.ingredients_container}>
       <div className={style.titles}>
         <h1 className="text text_type_main-large">{localize.CreateBurger}</h1>
       </div>
-      <Tabs/>
-      <div className={style.ingredients_scroll}>
-        <div className={style.titles}>
+      <div className={style.tabs_container}>
+        <Tab 
+          value={Ingredients.bun} 
+          active={activeTab === Ingredients.bun} 
+          onClick={() => { 
+            setActiveTab(Ingredients.bun);
+            scrollOnBunTabClick();
+          }}
+        >
+          {localize.Buns}
+        </Tab>
+        <Tab 
+          value={Ingredients.sauce} 
+          active={activeTab === Ingredients.sauce}
+          onClick={() => { 
+            setActiveTab(Ingredients.sauce);
+            scrollOnSauceTabClick();
+          }}
+        >
+          {localize.Sauces}
+        </Tab>
+        <Tab 
+          value={Ingredients.main}
+          active={activeTab === Ingredients.main} 
+          onClick={() => { 
+            setActiveTab(Ingredients.main);
+            scrollOnMainTabClick();
+          }}
+        >
+          {localize.Ingredients}
+        </Tab>
+      </div>
+      <div className={style.ingredients_scroll} onScroll={onScroll}>
+        <div className={style.titles} ref={bunTab}>
           <h1 className="text text_type_main-medium">{localize.Buns}</h1>
         </div>
         <div className={style.ingredients_box}>
@@ -37,7 +100,7 @@ const BurgerIngredients = () => {
             <Card key={bun._id} data={bun}/>
           ))}
         </div>
-        <div className={style.titles}>
+        <div className={style.titles} ref={sauceTab}>
           <h1 className="text text_type_main-medium">{localize.Sauces}</h1>
         </div>
         <div className={style.ingredients_box}>
@@ -45,7 +108,7 @@ const BurgerIngredients = () => {
             <Card key={sauce._id} data={sauce}/>
           ))}
         </div>
-        <div className={style.titles}>
+        <div className={style.titles} ref={mainTab}>
           <h1 className="text text_type_main-medium">{localize.Ingredients}</h1>
         </div>
         <div className={style.ingredients_box}>
